56a9fd10bdcc4797899508fe25d9c6be2c3370e7
Sunil Kim <progress0987@hanmail.net>
2017-12-05 15:46:00 +0900

    액션/버튼 처리

---
+++ API_Base/winAPI/Button.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "gameNode.h"
+//콜백함수는 함수포인터의 형태를 띈다. - 확인
+typedef void(*CALLBACK_FUNCTION)(void);
+enum BUTTONDIRECTION
+{
+	BUTTONDIRECTION_NULL,
+	BUTTONDIRECTION_UP,
+	BUTTONDIRECTION_DOWN,
+};
+class Button :public gameNode
+{
+private:
+	BUTTONDIRECTION Dir;
+	const char* imgName;
+	image* img;
+	RECT rc;
+	float X, Y;
+
+	POINT btnDownFramePoint;
+	POINT btnUpFramePoint;
+
+	CALLBACK_FUNCTION callbackfunction;
+
+public:
+
+	HRESULT init(const char* imgName, int x, int y, POINT btnDownFramePoint, POINT btnUpFramePoint, CALLBACK_FUNCTION cbFunc);
+	void release(void);
+	void update(void);
+	void render();
+	Button();
+	~Button();
+};
+
